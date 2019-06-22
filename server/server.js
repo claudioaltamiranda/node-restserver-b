062a29478f6702c823b593c2443fc1eef7399f16
Claudio Altamiranda <claudioaltamiranda@yahoo.com.ar>
2019-06-21 21:05:11 -0300

    Arreglado el problema del puerto en server.js

--- server/server.js
+++ server/server.js
@@ -36,6 +36,6 @@ app.delete('/usuario', function(req, res) {
     res.json('delete Usuario');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
 });
